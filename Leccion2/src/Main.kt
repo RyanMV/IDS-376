7c6bb5f122b11c5580fbcefde51a57da0736b246
RyanMV <rryyaannn2@gmail.com>
2024-04-03 22:19:24 -0400

    Update CodeLab 2

--- Leccion2/src/Main.kt
+++ Leccion2/src/Main.kt
@@ -64,6 +64,49 @@ fun main() {
     velocidadCanina("Lento")
     velocidadCanina(velocidad = "Veloz")
 
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+    println( decorations.filter {it[0] == 'p'})
+    val eager = decorations.filter { it [0] == 'p' }
+    println("eager: $eager")
 
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
 
+    val newList = filtered.toList()
+    println("new list: $newList")
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
+
+    fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+        return operation(dirty)
+    }
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+    println(updateDirty(30, waterFilter))
+    fun increaseDirty( start: Int ) = start + 1
+
+    println(updateDirty(15, ::increaseDirty))
+    var dirtyLevel = 19
+    dirtyLevel = updateDirty(dirtyLevel) { dirtyLevel -> dirtyLevel + 23}
+    println(dirtyLevel)
 }
